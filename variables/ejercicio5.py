e968eba667ec2d065720500142a5180e3e4d6b8c
danielamendozach <daniela.mendoza.c@ucb.edu.bo>
2020-09-29 17:13:00 -0400

    Mejora del ejercicio 5, poder ingresar números de tipo float.

--- variables/ejercicio5.py
+++ variables/ejercicio5.py
@@ -1,5 +1,5 @@
-horas_trabajadas= input("¿Cuantás horas has trabajado? ")
-paga_por_hora= input("¿Cuantó es la paga por hora? ")
-monto_a_pagar = int(horas_trabajadas) * int(paga_por_hora)
-resultado= "Monto total a pagar " + str(monto_a_pagar)
+horas_trabajadas= int(input("¿Cuantás horas has trabajado? "))
+paga_por_hora= float(input("¿Cuantó es la paga por hora? "))
+monto_a_pagar = horas_trabajadas * paga_por_hora
+resultado= "Monto total a pagar: " + str(monto_a_pagar)
 print(resultado)
