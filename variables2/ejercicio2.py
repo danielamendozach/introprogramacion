40f2125d7c28b40f671b519f7b84f57f10d6839b
danielamendozach <daniela.mendoza.c@ucb.edu.bo>
2020-10-01 16:26:46 -0400

    • E2: Leer dos números enteros por teclado, dividendo y divisor, y mostrar el resto de la división en pantalla. El mensaje de salida debe tener el siguiente formato: “El resto obtenido al dividir <dividendo> entre <divisor> es <resto_división>

---
+++ variables2/ejercicio2.py
@@ -0,0 +1,4 @@
+dividendo= int(input("Ingresar dividendo: "))
+divisor= int(input("Ingresar divisor: "))
+resto= int(dividendo % divisor)
+print( f"El resto obtenido al dividir {dividendo} entre {divisor} es {resto}")
