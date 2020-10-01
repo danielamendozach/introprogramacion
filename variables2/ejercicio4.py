13ce497ad2264b9299b0c8069a25e67fb1b79558
danielamendozach <daniela.mendoza.c@ucb.edu.bo>
2020-10-01 17:06:22 -0400

    E4: Leer dos números por teclado y mostrar en pantalla, para cada enunciado, si es verdadero o falso.

---
+++ variables2/ejercicio4.py
@@ -0,0 +1,10 @@
+num1= int(input("Ingresar número 1: "))
+num2= int(input("Ingresar número 2: "))
+iguales= num1 == num2
+print(f"Los números son iguales: {iguales}.")
+diferentes= num1 != num2
+print(f"Los números son diferentes: {diferentes}")
+num1_mayor= num1 > num2
+print(f"El primer número es mayor al segundo: {num1_mayor}")
+num1_menor_igual= num1 <= num2
+print(f"El primer número es menor o igual al segundo: {num1_menor_igual}")
