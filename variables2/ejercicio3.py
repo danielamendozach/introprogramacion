416697d17d554d9821423cf4584a94e99bb6e26b
danielamendozach <daniela.mendoza.c@ucb.edu.bo>
2020-10-01 16:41:15 -0400

    E3: Leer el radio de un círculo por teclado y mostrar el área y perímetro del círculo en pantalla.

---
+++ variables2/ejercicio3.py
@@ -0,0 +1,7 @@
+import math
+
+radio_círculo= float(input("Ingresar radio del círculo: "))
+área_círculo= math.pi * radio_círculo**2
+perímetro_círculo= 2 * math.pi * radio_círculo
+print(f"El área del círculo es igual a {área_círculo}.")
+print(f"El perímetro del círculo es igual a {perímetro_círculo}.")
