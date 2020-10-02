b5a28237fcc1fccc2f3b04ff7978d4a38f991698
danielamendozach <daniela.mendoza.c@ucb.edu.bo>
2020-10-02 16:57:32 -0400

    E3: Escribir un programa que permita al usuario votar por dos posibles partidos políticos, A o B. Leer el partido por el que vota el usuario por teclado y mostrar el mensaje „Usted ha votado por <partido>“. En caso de que el usuario ingrese una valor incorrecto, mostrar el mensaje „Voto inválido“.

---
+++ flujo_condicional/ejercicio3.py
@@ -0,0 +1,7 @@
+partido= input("Ingresar partido político: ")
+if partido==("A") or partido==("a"):
+    print(f"Usted ha votado por: {partido}")
+elif partido==("B") or partido==("b"):
+    print(f"Usted ha votado por: {partido}")
+else:
+    print("Voto Inválido")
