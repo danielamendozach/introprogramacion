fe091b033327cb9d3a1a4dba44a81af82ba059b9
danielamendozach <daniela.mendoza.c@ucb.edu.bo>
2020-10-02 16:41:08 -0400

    E2: Leer dos números enteros por teclado y mostrar el mayor de los dos por pantalla.

---
+++ flujo_condicional/ejercicio2.py
@@ -0,0 +1,6 @@
+num1= int(input("Ingrese el primer número: "))
+num2= int(input("Ingrese el segundo número: "))
+if num1 > num2:
+    print(f"El número mayor es: {num1}")
+else:
+    print(f"El número mayor es: {num2}")
