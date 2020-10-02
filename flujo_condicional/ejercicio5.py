9416832981bb99a3be016a8f051aa4645a2d77ef
danielamendozach <daniela.mendoza.c@ucb.edu.bo>
2020-10-02 17:12:56 -0400

    E5: Programar el diagrama de flujo.

---
+++ flujo_condicional/ejercicio5.py
@@ -0,0 +1,8 @@
+num= int(input("Ingrese el número: "))
+if num != 0:
+    if num < 0:
+        print("El número es negativo.")
+    else:
+        print("El número es positivo.")
+else:
+    print("El número es cero.")
