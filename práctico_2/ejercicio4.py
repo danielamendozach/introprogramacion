c313fc9f2661d854cdd343603cb880399fc66608
danielamendozach <daniela.mendoza.c@ucb.edu.bo>
2020-10-26 00:21:33 -0400

    Escribir una función que tome un carácter y devuelva True si es una vocal, de lo contrario devuelve False.

---
+++ práctico_2/ejercicio4.py
@@ -0,0 +1,7 @@
+def vocal(str):
+    if str=="a" or str=="e" or str=="i" or str=="o" or str=="u":
+        print(True)
+    else:
+        print(False)
+car=input("Ingrese un carácter: ")
+vocal(car)
