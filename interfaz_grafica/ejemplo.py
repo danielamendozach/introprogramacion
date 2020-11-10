d2a63cce99e3287938ba411867ca5ea45e59b352
danielamendozach <daniela.mendoza.c@ucb.edu.bo>
2020-11-10 17:42:28 -0400

    Ventana de texto, suma

---
+++ interfaz_grafica/ejemplo.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+from tkinter import *
+
+def limpiarcampos():
+    numA.set(0)
+    numB.set(0)
+def sumar():
+    numeroA=numA.get()
+    numeroB=numB.get()
+    resultado= numeroA+numeroB
+    Res.set(resultado)
+    limpiarcampos()
+#Creacion de ventana
+#Utilizamos el "tk" porque en la parte superior de la iumportacion de la libreria indicamos que
+#en vez de ultilizar la palabra "tkinter" utilizaremos "tk" por eso es el uso del as.
+ventana= tk.Tk()
+#Configurar el tamaño de la ventana
+ventana.config(width=300, height=200)
+etiquetaNA=Label(ventana,text="Número A: ")
+etiquetaNA.place(x=0,y=0)
+numA=IntVar()
+#Creacion de una entrada "caja de texto" con la palabra "ttk" que proviene de la libreria tkinter
+#Le indicamos tambien que esta entrada estara entro de nuestra ventana
+entradaNA=Entry(ventana, textvariable=numA)
+#Posteriormente le damos una posicion a esta entrada o caja de texto
+entradaNA.place(x=70,y=0)
+
+etiquetaNB=Label(ventana,text="Número B:")
+etiquetaNB.place(x=0,y=30)
+numB=IntVar()
+entradaNB=Entry(ventana, textvariable=numB)
+entradaNB.place(x=70,y=30)
+
+etiquetaRES=Label(ventana,text="Resultado:")
+etiquetaRES.place(x=0,y=60)
+Res=IntVar()
+salidaRES=Entry(ventana, textvariable=Res)
+salidaRES.place(x=70,y=60)
+
+botontransportar=Button(ventana, text="Sumar A y B:", command=sumar)
+botontransportar.place(x=0,y=90)
+
+ventana.mainloop()
