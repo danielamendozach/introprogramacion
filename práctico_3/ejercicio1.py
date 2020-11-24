d0b6426ee98a6bd1d3b5f97ede352451948dc0e7
danielamendozach <daniela.mendoza.c@ucb.edu.bo>
2020-11-24 16:29:55 -0400

    Convertido a función.

--- práctico_3/ejercicio1.py
+++ práctico_3/ejercicio1.py
@@ -1,6 +1,10 @@
-num_fac=1
-FACT = []
-for num in range(1,21):
-    num_fac = num*num_fac
-    FACT.append(num_fac)
-print(FACT)
+def numfactorial(numf):
+    num_fac = 1
+    FACT = []
+    for num in range(1, numf+1):
+        num_fac = num * num_fac
+        FACT.append(num_fac)
+    print(FACT)
+
+numfinal = 20
+numfactorial(numfinal)
