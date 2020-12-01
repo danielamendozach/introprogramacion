14eaa08d19f607d75d1c1313ad60fb8340f21a16
danielamendozach <daniela.mendoza.c@ucb.edu.bo>
2020-12-01 16:37:20 -0400

    Desarrollar un algoritmo donde se solicite un rango de valores, un valor inicial y un valor final. C.- Imprimir los valores impares que se encuentren dentro del rango y el valor de su sumatoria.

---
+++ funciones/ejercicio1_C.py
@@ -0,0 +1,14 @@
+def incisoC():
+    vector=[]
+    suma=0
+    for num in range(num1, num2+1):
+        if num%2!=0:
+            vector.append(num)
+            suma=suma+num
+    print("Inciso C:")
+    print("Valores:", vector)
+    print("Suma:", suma)
+
+num1=int(input("Ingrese el número inicial: "))
+num2=int(input("Ingrese el número final: "))
+incisoC()
