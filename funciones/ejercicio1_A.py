14d14d36bc8912dfc9c3f0bfad1898d8fc5434f6
danielamendozach <daniela.mendoza.c@ucb.edu.bo>
2020-12-01 16:35:35 -0400

    Desarrollar un algoritmo donde se solicite un rango de valores, un valor inicial y un valor final. A.- Imprimir todos los números naturales que se encuentren dentro de ese rango.

---
+++ funciones/ejercicio1_A.py
@@ -0,0 +1,10 @@
+def incisoA():
+    vector = []
+    for num in range(num1, num2 + 1):
+        vector.append(num)
+    print("Inciso A:")
+    print("Valores:", vector)
+
+num1=int(input("Ingrese el número inicial: "))
+num2=int(input("Ingrese el número final: "))
+incisoA()
